21e3297e02671cf9bfda0e238c3121b7511c44af
aberrantfox <aberrantfoxtph@gmail.com>
2018-06-10 18:28:22 +0100

    Initial implementation of HelpService

---
+++ src/main/kotlin/me/aberrantfox/kjdautils/internal/command/HelpService.kt
@@ -0,0 +1,68 @@
+package me.aberrantfox.kjdautils.internal.command
+
+import me.aberrantfox.kjdautils.api.dsl.Command
+import me.aberrantfox.kjdautils.api.dsl.CommandsContainer
+import me.aberrantfox.kjdautils.api.dsl.arg
+import me.aberrantfox.kjdautils.api.dsl.embed
+import me.aberrantfox.kjdautils.extensions.stdlib.randomListItem
+import me.aberrantfox.kjdautils.internal.command.arguments.CommandArg
+import java.awt.Color
+
+class HelpService(val container: CommandsContainer, val prefix: String) {
+
+    init {
+        val helpMenu = container.command("menu") {
+            description = "If you forget how something works, just use this command."
+            execute {
+                it.respond(embed{
+                    title("Help menu")
+                    field {
+                        name = "Wondering how a command works?"
+                        value = "${prefix}help <command name>, e.g. ${prefix}help help"
+                    }
+                })
+            }
+        }
+
+        container.command("help") {
+            expect(arg(CommandArg, true, helpMenu!!))
+            execute {
+                val command = it.args.component1() as Command
+                it.respond(generateEmbed(command))
+            }
+        }
+    }
+
+    private fun generateEmbed(command: Command) = embed {
+        title("Displaying help for ${command.name}")
+        description(command.description)
+        setColor(Color.CYAN)
+        val commandInvocation = "$prefix${command.name} "
+
+        field {
+            name = "What is the structure of the command?"
+            value = "$commandInvocation ${generateStructure(command)}"
+            inline = false
+        }
+
+        field {
+            name = "Show me an example of someone using the command."
+            value = "$commandInvocation ${generateExample(command)}"
+            inline = false
+        }
+    }
+
+    private fun generateStructure(command: Command) =
+            command.expectedArgs.joinToString(" ") {
+                if (it.optional) {
+                    "(${it.type.name})"
+                } else {
+                    "[${it.type.name}]"
+                }
+            }
+
+    private fun generateExample(command: Command) =
+            command.expectedArgs.joinToString(" ") {
+                it.type.examples.randomListItem()
+            }
+}
