1aef66af3fa81be2556b5686c31290e629d88cfd
Amish Naidu <amhndu@gmail.com>
2018-08-01 16:47:14 +0530

    Generalize ChoiceArg while keeping backwards compatibility
    It can now take any arbitrary choices, matching arguments that are equal
    to it's `.toString` string.
    Companion object which default to ChoiceArg(true, false) ensure
    backwards compatibility with the previous behavior of ChoiceArg

--- src/main/kotlin/me/aberrantfox/kjdautils/internal/command/arguments/ChoiceArg.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/internal/command/arguments/ChoiceArg.kt
@@ -7,11 +7,20 @@ import me.aberrantfox.kjdautils.internal.command.ArgumentResult
 import me.aberrantfox.kjdautils.internal.command.ArgumentType
 import me.aberrantfox.kjdautils.internal.command.ConsumptionType
 
-object ChoiceArg : ArgumentType {
-    override val examples = arrayListOf("true", "false")
+/**
+ * ChoiceArg takes in any number of arbitrary objects and maps arguments that match it's toString() string
+ * Example: the companion object allows two choices "true" or "false" and will automatically convert to Boolean
+ */
+open class ChoiceArg(vararg choices: Any) : ArgumentType {
+    companion object BinaryChoiceArg : ChoiceArg(true, false)
+
+    private val enumerations = choices.associateBy { it.toString().toLowerCase() }
+
+    override val examples = ArrayList(enumerations.keys)
     override val name = "Choice"
     override val consumptionType = ConsumptionType.Single
-    override fun isValid(arg: String, event: CommandEvent) = arg.isBooleanValue()
+    override fun isValid(arg: String, event: CommandEvent) = true
     override fun convert(arg: String, args: List<String>, event: CommandEvent) =
-            ArgumentResult.Single(arg.toBooleanValue())
+            enumerations[arg.toLowerCase()]?.let (ArgumentResult::Single) ?: ArgumentResult.Error(
+                    "Invalid choice. Available choices: ${enumerations.keys.joinToString (", ")}")
 }
