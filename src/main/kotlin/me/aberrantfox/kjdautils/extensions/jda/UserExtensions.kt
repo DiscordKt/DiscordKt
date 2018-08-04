6194a1ab8b3288e06f55334c69c6874f0bdc2a6f
Abzy <Alexx1103@AOL.com>
2018-08-05 01:25:00 +0200

    Updated UserExtensions
    sendPrivateMessage will now send a warning in the alert channel if a user doesn't allow direct messages from server members

--- src/main/kotlin/me/aberrantfox/kjdautils/extensions/jda/UserExtensions.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/extensions/jda/UserExtensions.kt
@@ -1,5 +1,6 @@
 package me.aberrantfox.kjdautils.extensions.jda
 
+import me.aberrantfox.kjdautils.internal.logging.BotLogger
 import net.dv8tion.jda.core.entities.Guild
 import net.dv8tion.jda.core.entities.MessageEmbed
 import net.dv8tion.jda.core.entities.User
@@ -7,15 +8,25 @@ import net.dv8tion.jda.core.entities.User
 
 fun User.toMember(guild: Guild) = guild.getMemberById(this.id)
 
-fun User.sendPrivateMessage(msg: MessageEmbed) =
-    openPrivateChannel().queue {
-        it.sendMessage(msg).queue()
-    }
+fun User.sendPrivateMessage(msg: MessageEmbed, log: BotLogger) =
+        try {
+            openPrivateChannel().queue {
+                it.sendMessage(msg).complete()
+            }
+        }
+        catch (e: Exception) {
+            log.alert("Couldn't open private chat with ${this.fullName()} since the user doesn't allow direct messages from server members.")
+        }
 
-fun User.sendPrivateMessage(msg: String) =
-    openPrivateChannel().queue {
-        it.sendMessage(msg).queue()
-    }
+fun User.sendPrivateMessage(msg: String, log: BotLogger) =
+        try {
+            openPrivateChannel().queue {
+                it.sendMessage(msg).complete()
+            }
+        }
+        catch (e: Exception) {
+            log.alert("Couldn't open private chat with ${this.fullName()} since the user doesn't allow direct messages from server members.")
+        }
 
 fun User.fullName() = "$name#$discriminator"
 
