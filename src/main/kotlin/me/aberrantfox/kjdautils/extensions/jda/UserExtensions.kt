c9cdd54179bbad915b6d3161aa670890a7a68d69
Abzy <Alexx1103@AOL.com>
2018-08-06 02:04:54 +0200

    Adjusted sendPrivateMessage functions so they will still be executed asynchronously

--- src/main/kotlin/me/aberrantfox/kjdautils/extensions/jda/UserExtensions.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/extensions/jda/UserExtensions.kt
@@ -9,25 +9,21 @@ import net.dv8tion.jda.core.entities.User
 fun User.toMember(guild: Guild) = guild.getMemberById(this.id)
 
 fun User.sendPrivateMessage(msg: MessageEmbed, log: BotLogger) =
-        try {
-            openPrivateChannel().queue {
-                it.sendMessage(msg).complete()
+        openPrivateChannel().queue {
+            it.sendMessage(msg).queue(null) {
+                notifyDirectMessageError(this.fullName(), log)
             }
-        }
-        catch (e: Exception) {
-            log.alert("Couldn't open private chat with ${this.fullName()} since the user doesn't allow direct messages from server members.")
         }
 
 fun User.sendPrivateMessage(msg: String, log: BotLogger) =
-        try {
-            openPrivateChannel().queue {
-                it.sendMessage(msg).complete()
-            }
+        openPrivateChannel().queue {
+            it.sendMessage(msg).queue(null) {
+                notifyDirectMessageError(this.fullName(), log)
         }
-        catch (e: Exception) {
-            log.alert("Couldn't open private chat with ${this.fullName()} since the user doesn't allow direct messages from server members.")
-        }
+    }
 
 fun User.fullName() = "$name#$discriminator"
 
 fun User.descriptor() = "${fullName()} :: ID :: $id"
+
+fun notifyDirectMessageError(user: String, log: BotLogger) = log.alert("Couldn't open private chat with $user since the user doesn't allow direct messages from server members.")
