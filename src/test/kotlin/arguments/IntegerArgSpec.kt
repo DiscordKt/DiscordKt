680d4c77cb3478664319d07ad0d827849afba5e4
AberrantFox <aberrantfoxtph@gmail.com>
2018-11-17 13:56:16 +0000

    Refactor of tests and addition of double tests

--- src/test/kotlin/arguments/IntegerArgSpec.kt
+++ src/test/kotlin/arguments/IntegerArgSpec.kt
@@ -4,6 +4,8 @@ import io.mockk.mockk
 import me.aberrantfox.kjdautils.api.dsl.CommandEvent
 import me.aberrantfox.kjdautils.internal.command.ArgumentResult
 import me.aberrantfox.kjdautils.internal.command.arguments.IntegerArg
+import mock.converToError
+import mock.convertToSingle
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 import kotlin.test.assertEquals
@@ -12,25 +14,25 @@ object IntegerArgSpec: Spek({
     Feature("Integer Command Argument") {
         Scenario("Passing 3 to be converted") {
             Then("3 is correctly parsed into the correct type and value ") {
-                assertEquals(3, convertArgToSingle("3"))
+                assertEquals(3, IntegerArg.convertToSingle("3"))
             }
         }
 
         Scenario("Passing Integer.MAX_VALUE to be converted") {
             Then("Integer.MAX_VALUE is correctly returned as an integer, as the correct value") {
-                assertEquals(Integer.MAX_VALUE, convertArgToSingle("${Integer.MAX_VALUE}"))
+                assertEquals(Integer.MAX_VALUE, IntegerArg.convertToSingle("${Integer.MAX_VALUE}"))
             }
         }
 
         Scenario("A double value is passed to be converted") {
             Then("The Conversion fails") {
-                assertEquals(ArgumentResult.Error::class.java, convertArgToError("2.3")::class.java)
+                assertEquals(ArgumentResult.Error::class.java, IntegerArg.converToError("2.3")::class.java)
             }
         }
 
         Scenario("A blank value is passed to be converted") {
             Then("The Conversion fails") {
-                assertEquals(ArgumentResult.Error::class.java, convertArgToError("")::class.java)
+                assertEquals(ArgumentResult.Error::class.java, IntegerArg.converToError("")::class.java)
             }
         }
     }
