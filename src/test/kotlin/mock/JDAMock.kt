4702ab429d9536b094cd66e2b14a3ab0d0f261aa
AberrantFox <aberrantfoxtph@gmail.com>
2018-11-18 17:56:51 +0000

    Addition of more mocks as well as Command Argument Tests

--- src/test/kotlin/mock/JDAMock.kt
+++ src/test/kotlin/mock/JDAMock.kt
@@ -3,7 +3,9 @@ package mock
 
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import me.aberrantfox.kjdautils.api.dsl.Command
 import me.aberrantfox.kjdautils.api.dsl.CommandEvent
+import me.aberrantfox.kjdautils.api.dsl.CommandsContainer
 import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.entities.Category
 
@@ -16,6 +18,16 @@ val jdaMock = mock<JDA> {
     on { getCategoryById("1") } doReturn categoryMock
 }
 
+val pingCommandMock = mock<Command> {
+    on { name } doReturn "ping"
+    on { execute } doReturn {}
+}
+
+val commandContainerMock = mock<CommandsContainer> {
+    on { get("ping") } doReturn pingCommandMock
+}
+
 val commandEventMock = mock<CommandEvent> {
     on { jda } doReturn jdaMock
+    on { container } doReturn commandContainerMock
 }
