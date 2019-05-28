5c91fea732c9a47de44231af8f16a7dd38d5ea9e
ianagbip1oti <ianagbip1oti@gmail.com>
2019-05-28 18:10:45 +0000

    Remove JDA from EventRegister

--- src/main/kotlin/me/aberrantfox/kjdautils/discord/KJDA.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/discord/KJDA.kt
@@ -3,14 +3,25 @@ package me.aberrantfox.kjdautils.discord
 import me.aberrantfox.kjdautils.api.dsl.KConfiguration
 import me.aberrantfox.kjdautils.extensions.jda.*
 import me.aberrantfox.kjdautils.internal.logging.BotLogger
+import me.aberrantfox.kjdautils.internal.event.EventRegister
 
 import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
 import net.dv8tion.jda.core.entities.User as JDAUser
 import net.dv8tion.jda.core.entities.MessageEmbed
+import net.dv8tion.jda.core.events.Event
+import net.dv8tion.jda.core.hooks.EventListener
 
 class KJDA(override val jda: JDA) : Discord {
+
+  override fun addEventListener(er: EventRegister): Unit {
+    jda.addEventListener(object: EventListener {
+      override fun onEvent(evt: Event) {
+        er.onEvent(evt)
+      }
+    })
+  }
 
   override fun getUserById(userId: String): User {
     return jda.getUserById(userId).fromJDA()
