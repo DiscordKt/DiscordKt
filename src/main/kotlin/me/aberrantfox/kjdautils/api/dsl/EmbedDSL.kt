7e82b02f7a9b59d5c68e4473b354c716ebac1a51
ianagbip1oti <ianagbip1oti@gmail.com>
2019-07-14 04:31:20 +0000

    Remove the extends EmbedBuilder
    This also has the effect of hiding many methods that may have previously
    been available. It's unclear how many would actually be in use, but
    could be added when needed

--- src/main/kotlin/me/aberrantfox/kjdautils/api/dsl/EmbedDSL.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/api/dsl/EmbedDSL.kt
@@ -4,25 +4,58 @@ import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.entities.MessageEmbed
 import java.awt.Color
 
-class EmbedDSLHandle : EmbedBuilder() {
+class EmbedDSLHandle {
+    private var title : String? = null
+    private var description : String? = null
+    private var color : Color? = null
+    private var thumbnail : String? = null
+    private var fields : MutableList<FieldStore> = mutableListOf()
+
     operator fun invoke(args: EmbedDSLHandle.() -> Unit) {}
 
-    fun title(title: String?) = this.setTitle(title)
+    fun title(t: String?) = { title = t }
+    fun description(d: String?) = { description = d }
+    fun color(c: Color) = { color = c }
+    fun thumbnail(t: String?) = { thumbnail = t }
 
-    fun description(descr: String?) = this.setDescription(descr)
+    @Deprecated("use title")
+    fun setTitle(t: String?) : EmbedDSLHandle = apply { title(t) }
 
-    fun color(color: Color) = this.setColor(color)
+    @Deprecated("use description")
+    fun setDescription(d: String?) : EmbedDSLHandle = apply { description(d) }
+
+    @Deprecated("use color")
+    fun setColor(c: Color) : EmbedDSLHandle = apply { color(c) }
+
+    @Deprecated("use thumbnail")
+    fun setThumbnail(t: String?) : EmbedDSLHandle = apply { thumbnail(t) }
+
+    fun addBlankField(inl: Boolean) = field {
+        inline = inl
+    }
 
     fun field(construct: FieldStore.() -> Unit) {
         val field = FieldStore()
         field.construct()
-        addField(field.name, field.value, field.inline)
+        fields.add(field)
     }
 
     fun ifield(construct: FieldStore.() -> Unit) {
         val field = FieldStore()
         field.construct()
-        addField(field.name, field.value, true)
+        fields.add(field)
+    }
+
+    fun build() : MessageEmbed {
+        val b = EmbedBuilder()
+        title.let { b.setTitle(it) }
+        description.let { b.setDescription(it) }
+        color.let { b.setColor(it) }
+        thumbnail.let { b.setThumbnail(it) }
+
+        fields.forEach { b.addField(it.name, it.value, it.inline) }
+
+        return b.build()
     }
 }
 
