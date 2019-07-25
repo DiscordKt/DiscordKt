ddb763bb623134876440eca0e0db42e0ffbca488
Elliott Short <elliott.short@gmail.com>
2019-07-24 21:26:56 -0500

    Update UserArg.

--- src/main/kotlin/me/aberrantfox/kjdautils/internal/command/arguments/UserArg.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/internal/command/arguments/UserArg.kt
@@ -6,16 +6,20 @@ import me.aberrantfox.kjdautils.internal.command.ArgumentResult
 import me.aberrantfox.kjdautils.internal.command.ArgumentType
 import me.aberrantfox.kjdautils.internal.command.ConsumptionType
 import me.aberrantfox.kjdautils.internal.command.tryRetrieveSnowflake
+import net.dv8tion.jda.api.entities.User
 
-open class UserArg(override val name: String = "User", val allowsBot: Boolean = false) : ArgumentType {
+open class UserArg(override val name: String = "User", private val allowsBot: Boolean = false) : ArgumentType {
     companion object : UserArg()
 
     override val examples = arrayListOf("@Bob", "268856125007331328", "275544730887127040")
     override val consumptionType = ConsumptionType.Single
     override fun convert(arg: String, args: List<String>, event: CommandEvent): ArgumentResult {
-        val user = event.discord.jda.getUserById(arg.trimToID()) ?: return ArgumentResult.Error("Couldn't retrieve user: $arg")
+        val user = tryRetrieveSnowflake(event.discord.jda) {
+            it.retrieveUserById(arg.trimToID()).complete()
+        } as User? ?: return ArgumentResult.Error("Couldn't retrieve user: $arg")
 
-        if (!allowsBot && user.isBot) return ArgumentResult.Error("A bot is not a valid user arg.")
+        if (!allowsBot && user.isBot)
+            return ArgumentResult.Error("A bot is not a valid user arg.")
 
         return ArgumentResult.Single(user)
     }
