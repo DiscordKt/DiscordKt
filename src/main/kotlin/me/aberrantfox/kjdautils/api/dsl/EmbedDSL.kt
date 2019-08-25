5b7090e2a795c3b29006f6199ee22bb7161d2ade
Jake Mattson <JakeJMattson@gmail.com>
2019-08-25 16:59:48 -0500

    Remove Deprecated methods

--- src/main/kotlin/me/aberrantfox/kjdautils/api/dsl/EmbedDSL.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/api/dsl/EmbedDSL.kt
@@ -4,8 +4,6 @@ import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.MessageEmbed
 import java.awt.Color
 
-private const val deprecationMessage = "Use property access. EOL 0.11.x"
-
 class EmbedDSLHandle {
     var title : String? = null
     var description : String? = null
@@ -14,18 +12,6 @@ class EmbedDSLHandle {
     var fields : MutableList<FieldStore> = mutableListOf()
 
     operator fun invoke(args: EmbedDSLHandle.() -> Unit) {}
-
-    @Deprecated(deprecationMessage, ReplaceWith("apply { this.title = t }"), DeprecationLevel.WARNING)
-    fun setTitle(t: String?) : EmbedDSLHandle = apply { this.title = t }
-
-    @Deprecated(deprecationMessage, ReplaceWith("apply { this.description = d }"), DeprecationLevel.WARNING)
-    fun setDescription(d: String?) : EmbedDSLHandle = apply { this.description = d }
-
-    @Deprecated(deprecationMessage, ReplaceWith("apply { this.color = c }"), DeprecationLevel.WARNING)
-    fun setColor(c: Color) : EmbedDSLHandle = apply { this.color = c }
-
-    @Deprecated(deprecationMessage, ReplaceWith("apply { this.thumbnail = t }"), DeprecationLevel.WARNING)
-    fun setThumbnail(t: String?) : EmbedDSLHandle = apply { this.thumbnail = t }
 
     fun field(construct: FieldStore.() -> Unit) {
         val field = FieldStore()
