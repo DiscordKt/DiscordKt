d8388fca4801de8766d05cf5edb043d993d7eeb8
Jake Mattson <JakeJMattson@gmail.com>
2019-10-20 10:57:19 -0500

    Modify HexColorArg and provide test

--- src/main/kotlin/me/aberrantfox/kjdautils/internal/arguments/HexColorArg.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/internal/arguments/HexColorArg.kt
@@ -2,19 +2,24 @@ package me.aberrantfox.kjdautils.internal.arguments
 
 import me.aberrantfox.kjdautils.api.dsl.command.CommandEvent
 import me.aberrantfox.kjdautils.internal.command.*
+import java.awt.Color
 
-open class HexColorArg(override val name : String = "Hex Color"): ArgumentType<Int>() {
+open class HexColorArg(override val name : String = "Hex Color"): ArgumentType<Color>() {
     companion object : HexColorArg()
 
-    override val examples = arrayListOf("#000000", "FFFF00", "#3498db", "db3434")
+    override val examples = arrayListOf("#000000", "ffffff")
     override val consumptionType = ConsumptionType.Single
-    override fun convert(arg: String, args: List<String>, event: CommandEvent<*>): ArgumentResult<Int> {
-        val error = "Invalid color argument."
+    override fun convert(arg: String, args: List<String>, event: CommandEvent<*>): ArgumentResult<Color> {
+        if (arg.length !in 6..7) return ArgumentResult.Error("Invalid format. Hex colors are 6 digits in length.")
 
-        if (arg.length !in 6..7) return ArgumentResult.Error(error)
+        val trimmedInput = arg.takeLast(6).toUpperCase()
+        val isValidHex = trimmedInput.all { it in '0'..'9' || it in 'A'..'F'}
 
-        val int = try { arg.takeLast(6).toInt(16) } catch (e: NumberFormatException) { return ArgumentResult.Error(error) }
+        if (!isValidHex)
+            return ArgumentResult.Error("Invalid format. Hexadecimal numbers range from 0 to F")
 
-        return if (int >= 0) ArgumentResult.Success(int) else ArgumentResult.Error(error)
+        val int = trimmedInput.toInt(16)
+
+        return ArgumentResult.Success(Color(int))
     }
 }
---
+++ src/test/kotlin/arguments/HexColorArg.kt
@@ -0,0 +1,19 @@
+package arguments
+
+import me.aberrantfox.kjdautils.internal.arguments.HexColorArg
+import utilities.SimpleArgTest
+import java.awt.Color
+
+class HexColorArgTest : SimpleArgTest {
+    override val argumentType = HexColorArg
+
+    override val validArgs = listOf(
+        "#000000" to Color(0x000000),
+        "000000" to Color(0x000000),
+        "#FFFFFF" to Color(0xFFFFFF),
+        "FFFFFF" to Color(0xFFFFFF),
+        "#00bFfF" to Color(0x00BFFF)
+    )
+
+    override val invalidArgs = listOf("black", "0", "#gggggg", "FFFFFFFF")
+}
