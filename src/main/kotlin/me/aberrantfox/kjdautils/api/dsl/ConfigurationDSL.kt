76011ac5a8948254603e5932af6d8c99c84b81db
JakeJMattson <JakeJMattson@gmail.com>
2020-04-18 23:43:48 -0500

    Convert visibilityPredicate to function

--- src/main/kotlin/me/aberrantfox/kjdautils/api/dsl/ConfigurationDSL.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/api/dsl/ConfigurationDSL.kt
@@ -10,6 +10,8 @@ enum class PrefixDeleteMode {
     None
 }
 
+data class VisibilityContext(val command: Command, val user: User, val channel: MessageChannel, val guild: Guild?)
+
 data class KConfiguration(
     var prefix: String = "+",
     var reactToCommands: Boolean = true,
@@ -17,13 +19,20 @@ data class KConfiguration(
     var deleteErrors: Boolean = false,
     var allowPrivateMessages: Boolean = false,
     internal var mentionEmbed: ((GuildMessageReceivedEvent) -> MessageEmbed)? = null,
-    var visibilityPredicate: (command: Command, User, MessageChannel, Guild?) -> Boolean = { _, _, _, _ -> true }
+    internal var visibilityPredicate: (command: Command, User, MessageChannel, Guild?) -> Boolean = { _, _, _, _ -> true }
 ) {
     fun mentionEmbed(construct: EmbedDSLHandle.(GuildMessageReceivedEvent) -> Unit) {
         mentionEmbed = {
             val handle = EmbedDSLHandle()
             handle.construct(it)
             handle.build()
+        }
+    }
+
+    fun visibilityPredicate(predicate: (VisibilityContext) -> Boolean = { _ -> true }) {
+        visibilityPredicate = { command, user, messageChannel, guild ->
+            val context = VisibilityContext(command, user, messageChannel, guild)
+            predicate.invoke(context)
         }
     }
 }
