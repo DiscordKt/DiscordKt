f46df35f4ae8f44b5a970538b3c9d9bc5b8d0306
JakeJMattson <JakeJMattson@gmail.com>
2020-04-20 11:34:25 -0500

    Buff examples

--- src/main/kotlin/me/aberrantfox/kjdautils/examples/MainApp.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/examples/MainApp.kt
@@ -21,21 +21,37 @@ fun main(args: Array<String>) {
 
     startBot(token) {
         configure {
+            //The prefix for commands that your bot will respond to
             prefix = "!"
-            allowMentionPrefix = true
 
+            //Whether or not mentioning the bot can be used as a prefix
+            allowMentionPrefix = false
+
+            //The emoji that the bot will react on invocations; null for none
+            commandReaction = "\uD83D\uDC40"
+
+            //Which invocations should be deleted (by number of prefixes)
+            deleteMode = PrefixDeleteMode.None
+
+            //Whether or not error messages should be deleted after sending
+            deleteErrors = false
+
+            //Whether or not commands in direct messages are valid
+            allowPrivateMessages = false
+
+            //Color configuration for embeds within KUtils
             colors {
                 successColor = Color.GREEN
                 failureColor = Color.RED
                 infoColor = Color.BLUE
             }
 
+            //An embed produced when the bot is mentioned
             mentionEmbed { event ->
                 val self = event.guild.jda.selfUser
 
                 color = Color(0x00bfff)
                 thumbnail = self.effectiveAvatarUrl
-                addField(self.fullName(), "This is an example embed that can be created whenever the bot is pinged.")
                 addInlineField("Prefix", prefix)
 
                 with(project) {
@@ -46,6 +62,11 @@ fun main(args: Array<String>) {
 
                     addInlineField("Source", repository)
                 }
+            }
+
+            //A predicate to determine if a command is visible in this context
+            visibilityPredicate {
+                it.command.names.first().length < 50
             }
         }
     }
