106793e8a8e47189a82416fed63ff7e484fb2d01
JakeJMattson <JakeJMattson@gmail.com>
2020-04-26 23:12:24 -0500

    Rewrite multi-word parser

--- src/main/kotlin/me/aberrantfox/kjdautils/internal/arguments/RoleArg.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/internal/arguments/RoleArg.kt
@@ -17,36 +17,28 @@ open class RoleArg(override val name : String = "Role", private val guildId: Str
         //If the arg is an ID, resolve it here, otherwise resolve by name
         if (arg.trimToID().isLong()) {
             val role = event.discord.jda.getRoleById(arg.trimToID())
-                ?: return ArgumentResult.Error("Could not resolve role by ID.")
 
-            return ArgumentResult.Success(role)
+            if (role != null)
+                return ArgumentResult.Success(role)
         }
 
-        var roles = guild.roles
-        val roleBuilder = StringBuilder()
-        fun String.startsWithIgnoreCase(string: String) = this.toLowerCase().startsWith(string.toLowerCase())
+        val argString = args.joinToString(" ").toLowerCase()
+        val viableNames = guild.roles
+            .filter { argString.startsWith(it.name.toLowerCase()) }
+            .sortedBy { it.name.length }
 
-        //Consume arguments until only one role matches the filter
-        args.takeWhile {
-            val padding = if (roleBuilder.isNotEmpty()) " " else ""
-            roleBuilder.append("$padding$it")
-            roles = roles.filter { it.name.startsWithIgnoreCase(roleBuilder.toString()) }
+        val longestMatch = viableNames.lastOrNull()
+        val result = longestMatch?.let { viableNames.filter { it.name == longestMatch.name } } ?: emptyList()
 
-            roles.size > 1
+        return when (result.size) {
+            0 -> ArgumentResult.Error("Could not resolve any roles by name.")
+            1 -> {
+                val role = result.first()
+                val argList = args.take(role.name.split(" ").size)
+                ArgumentResult.Success(role, argList)
+            }
+            else -> ArgumentResult.Error("Resolving role by name returned multiple matches. Please use an ID.")
         }
-
-        //Get the single role that survived filtering
-        val resolvedRole = roles.firstOrNull() ?: return ArgumentResult.Error("Couldn't retrieve role :: $roleBuilder")
-        val resolvedName = resolvedRole.name
-
-        //Determine how many args this role would consume
-        val lengthOfRole = resolvedName.split(" ").size
-
-        //Check if the role that survived filtering matches the args given
-        val argList = args.take(lengthOfRole)
-        val isValid = resolvedName.toLowerCase() == argList.joinToString(" ").toLowerCase()
-
-        return if (isValid) ArgumentResult.Success(resolvedRole, argList) else ArgumentResult.Error("Couldn't retrieve role :: $roleBuilder")
     }
 
     override fun generateExamples(event: CommandEvent<*>) =
