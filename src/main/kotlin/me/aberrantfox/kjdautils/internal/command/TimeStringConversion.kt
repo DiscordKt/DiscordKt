fce83f4f45f392fb6c59b963d9cffd7c4f78b028
JakeJMattson <JakeJMattson@gmail.com>
2020-05-06 23:38:04 -0500

    Add new quantifiers to TimeStringArg

--- src/main/kotlin/me/aberrantfox/kjdautils/internal/command/TimeStringConversion.kt
+++ src/main/kotlin/me/aberrantfox/kjdautils/internal/command/TimeStringConversion.kt
@@ -1,116 +1,105 @@
 package me.aberrantfox.kjdautils.internal.command
 
-import me.aberrantfox.kjdautils.extensions.stdlib.*
-import me.aberrantfox.kjdautils.internal.command.ArgumentResult.*
+import me.aberrantfox.kjdautils.extensions.stdlib.isDigitOrPeriod
 
 private typealias Quantity = Double
 private typealias Quantifier = String
 
 fun convertTimeString(actual: List<String>): ArgumentResult<Double> {
-
-    val possibleEnd = actual.indexOfFirst { toTimeElement(it) == null }
-
-    val timeStringEnd =
-            if (possibleEnd != -1) {
-                possibleEnd
-            } else {
-                actual.size
-            }
-
+    val timeStringEnd = actual.indexOfFirst { toTimeElement(it) == null }.takeIf { it != -1 } ?: actual.size
     val original = actual.subList(0, timeStringEnd).toList()
-
-    val possibleElements = original
-            .map(String::toLowerCase)
-            .map(::toTimeElement)
-
+    val possibleElements = original.map { toTimeElement(it.toLowerCase()) }
     val timeElements = possibleElements.dropLastWhile { it is Quantity } // assume trailing numbers are part of next arg (ID, Integer, etc.)
 
-    if (timeElements.isEmpty()) {
-        return Error("Invalid time element passed.")
-    }
+    if (timeElements.isEmpty())
+        return ArgumentResult.Error("Invalid time element passed.")
 
     val consumed = original.subList(0, timeElements.size)
-
     val quantityCount = timeElements.count { it is Quantity }
     val quantifierCount = timeElements.count { it is Quantifier }
 
-    if (quantityCount != quantifierCount) {
-        return Error("The number of quantities doesn't match the number of quantifiers.")
-    }
+    if (quantityCount != quantifierCount)
+        return ArgumentResult.Error("The number of quantities doesn't match the number of quantifiers.")
 
     val hasMissingQuantifier = timeElements.withIndex().any { (index, current) ->
         val next = timeElements.getOrNull(index + 1)
-
         current is Quantity && next !is Quantifier
     }
 
-    if (hasMissingQuantifier) {
-        return Error("At least one quantity is missing a quantifier.")
-    }
+    if (hasMissingQuantifier)
+        return ArgumentResult.Error("At least one quantity is missing a quantifier.")
 
-    val timeInSeconds = timeElements.asSequence().withIndex()
-            .mapNotNull { (index, element) ->
-                when (element) {
-                    is Pair<*, *> -> element
-                    is Quantity -> Pair(element, timeElements[index + 1])
-                    else -> null // e.g. quantifiers
-                }
+    val timeInSeconds = timeElements
+        .mapIndexedNotNull { index, element ->
+            when (element) {
+                is Pair<*, *> -> element as Pair<Quantity, Quantifier>
+                is Quantity -> element to timeElements[index + 1] as Quantifier
+                else -> null
             }
-            .map { it as Pair<Quantity, Quantifier> }
-            .map { (quantity, quantifier) -> quantity * timeStringToSeconds.getValue(quantifier) }
-            .reduce { a, b -> a + b }
+        }
+        .map { (quantity, quantifier) -> quantity * timeStringToSeconds.getValue(quantifier) }
+        .reduce { a, b -> a + b }
 
-    return Success(timeInSeconds, consumed)
+    return ArgumentResult.Success(timeInSeconds, consumed)
 }
 
 private fun toTimeElement(element: String): Any? {
     val both = toBoth(element)
-
     if (both != null) return both
 
     val quantifier = toQuantifier(element)
-
     if (quantifier != null) return quantifier
 
     val quantity = toQuantity(element)
-
     if (quantity != null) return quantity
 
     return null
 }
 
-private fun toQuantifier(element: String) = if (timeStringToSeconds.containsKey(element)) element else null
+private fun toQuantifier(element: String) = element.takeIf { it in timeStringToSeconds }
 
 private fun toQuantity(element: String) = element.toDoubleOrNull()
 
 private fun toBoth(element: String): Pair<Double, String>? {
-    val quantity = element.toCharArray().takeWhile { it.isDigitOrPeriod() }.joinToString("")
-    val quantifier = element.substring(quantity.length)
+    val quantityRaw = element.toCharArray().takeWhile { it.isDigitOrPeriod() }.joinToString("")
+    val quantity = toQuantity(quantityRaw) ?: return null
+    val quantifier = toQuantifier(element.substring(quantityRaw.length)) ?: return null
 
-    if (!(timeStringToSeconds.containsKey(quantifier))) return null
-
-    if (!(quantity.isDouble())) return null
-
-    return Pair(quantity.toDouble(), quantifier)
+    return quantity to quantifier
 }
 
 private val timeStringToSeconds = mapOf(
-        "second" to 1,
-        "seconds" to 1,
-        "s" to 1,
-        "sec" to 1,
-        "h" to 3600,
-        "hour" to 3600,
-        "hours" to 3600,
-        "minute" to 60,
-        "minutes" to 60,
-        "m" to 60,
-        "mins" to 60,
-        "min" to 60,
-        "days" to 86400,
-        "d" to 86400,
-        "day" to 86400,
-        "weeks" to 604800,
-        "w" to 604800,
-        "week" to 604800
+    "s" to 1,
+    "sec" to 1,
+    "second" to 1,
+    "seconds" to 1,
+
+    "m" to 60,
+    "min" to 60,
+    "mins" to 60,
+    "minute" to 60,
+    "minutes" to 60,
+
+    "h" to 3600,
+    "hr" to 3600,
+    "hrs" to 3600,
+    "hour" to 3600,
+    "hours" to 3600,
+
+    "d" to 86400,
+    "day" to 86400,
+    "days" to 86400,
+
+    "w" to 604800,
+    "week" to 604800,
+    "weeks" to 604800,
+
+    "month" to 2592000,
+    "months" to 2592000,
+
+    "y" to 31104000,
+    "yr" to 31104000,
+    "yrs" to 31104000,
+    "year" to 31104000,
+    "years" to 31104000
 )
--- src/test/kotlin/arguments/TimeStringArg.kt
+++ src/test/kotlin/arguments/TimeStringArg.kt
@@ -8,6 +8,8 @@ private const val minute = 60.0
 private const val hour = 3600.0
 private const val day = 86400.0
 private const val week = 604800.0
+private const val month = 2592000.0
+private const val year = 31104000.0
 
 class TimeStringArgTest : ArgumentTestFactory {
     override val argumentType = TimeStringArg
@@ -25,6 +27,8 @@ class TimeStringArgTest : ArgumentTestFactory {
         "1 minutes" to minute,
 
         "1h" to hour,
+        "1 hr" to hour,
+        "1 hrs" to hour,
         "1 hour" to hour,
         "1 hours" to hour,
 
@@ -36,11 +40,20 @@ class TimeStringArgTest : ArgumentTestFactory {
         "1 week" to week,
         "1 weeks" to week,
 
+        "1 month" to month,
+        "1 months" to month,
+
+        "1y" to year,
+        "1 yr" to year,
+        "1 yrs" to year,
+        "1 year" to year,
+        "1 years" to year,
+
         "5s" to second * 5,
         "5.5h" to hour * 5.5,
         "10 minutes 8 seconds" to (10 * minute) + (8 * second),
         "1h 2m 10 seconds" to (hour) + (2 * minute) + (10 * second),
-        "1w 1d 1h 1m 1s" to week + day + hour + minute + second
+        "1y 1 month 1w 1d 1hr 1minutes 1s" to year + month + week + day + hour + minute + second
     )
 
     override val invalidArgs = listOf("5", "-5m", "hour", "1m1s", "")
