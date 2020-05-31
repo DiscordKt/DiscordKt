bf1640be0d41cbd01f49b018d3a36588d18703dd
JakeJMattson <JakeJMattson@gmail.com>
2020-05-31 00:51:10 -0500

    Create buildSrc and constants file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,16 @@
-import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
-
-val projectName = "KUtils"
-
 group = "me.jakejmattson"
 version = "0.17.0"
 
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version Versions.kotlin
+    `java-library`
     `maven-publish`
     id("com.github.ben-manes.versions") version "0.28.0"
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
+    mavenLocal()
     jcenter()
 
     maven {
@@ -22,49 +19,53 @@ repositories {
 }
 
 dependencies {
+    //Internal Dependencies
+    implementation(Dependencies.coroutines)
+    implementation(Dependencies.reflections)
+    implementation(Dependencies.gson)
+    implementation(Dependencies.guava)
+    implementation(Dependencies.commons)
+    implementation(Dependencies.slf4j)
+
     //Library Dependencies
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
-    implementation("org.reflections:reflections:0.9.12")
-    implementation("net.dv8tion:JDA:4.1.1_155")
-    implementation("com.google.code.gson:gson:2.8.6")
-    implementation("com.google.guava:guava:29.0-jre")
-    implementation("org.apache.commons:commons-text:1.8")
-    implementation("org.slf4j:slf4j-simple:1.7.30")
+    implementation(Dependencies.jda)
 
     //Scripting Engine
-    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:${getKotlinPluginVersion()}")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:${getKotlinPluginVersion()}")
-    implementation("org.jetbrains.kotlin:kotlin-script-runtime:${getKotlinPluginVersion()}")
-    implementation("org.jetbrains.kotlin:kotlin-script-util:${getKotlinPluginVersion()}")
+    implementation(Dependencies.kotlinCompiler)
+    implementation(Dependencies.script_compiler)
+    implementation(Dependencies.script_runtime)
+    implementation(Dependencies.script_util)
 
     //Test Dependencies
-    testImplementation("io.mockk:mockk:1.10.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0-M1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0-M1")
+    testImplementation(Dependencies.mockk)
+    testImplementation(Dependencies.junit_api)
+    testRuntimeOnly(Dependencies.junit_engine)
 }
 
-tasks.compileJava {
-    options.encoding = "UTF-8"
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
 
-    java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        java {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
     }
-}
 
-tasks.test {
-    useJUnitPlatform()
+    test {
+        useJUnitPlatform()
+    }
 }
 
 publishing {
     publications {
-        create<MavenPublication>(projectName) {
-            artifactId = projectName
+        create<MavenPublication>(Constants.projectName) {
+            artifactId = Constants.projectName
             from(components["kotlin"])
             pom {
-                name.set(projectName)
-                description.set("A Kotlin wrapper for the Discord API.")
-                url.set("https://gitlab.com/JakeJMattson/$projectName/")
+                name.set(Constants.projectName)
+                description.set(Constants.projectDescription)
+                url.set(Constants.projectUrl)
                 licenses {
                     license {
                         name.set("MIT")
