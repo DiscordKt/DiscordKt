cf40138ad74c20f4edb3680b8b32d271da123f51
JakeJMattson <JakeJMattson@gmail.com>
2020-08-09 23:51:12 -0500

    Sanitize everyone/here

--- src/main/kotlin/me/jakejmattson/discordkt/api/extensions/stdlib/StringExtensions.kt
+++ src/main/kotlin/me/jakejmattson/discordkt/api/extensions/stdlib/StringExtensions.kt
@@ -6,8 +6,8 @@ import me.jakejmattson.discordkt.api.Discord
 import me.jakejmattson.discordkt.api.extensions.jda.fullName
 
 private val urlRegexes = listOf(
-    "[-a-zA-Z0-9@:%._+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&//=]*)",
-    "https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&//=]*)"
+        "[-a-zA-Z0-9@:%._+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&//=]*)",
+        "https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&//=]*)"
 ).map { it.toRegex() }
 
 private val inviteRegex = "(\n|.)*((discord|discordapp).(gg|me|io|com/invite)/)(\n|.)*".toRegex()
@@ -26,13 +26,13 @@ fun String.containsInvite() = inviteRegex.matches(this)
  * Whether or not this string is a valid boolean value (true/false/t/f).
  */
 fun String.isBooleanValue() =
-    when (this.toLowerCase()) {
-        "true" -> true
-        "false" -> true
-        "t" -> true
-        "f" -> true
-        else -> false
-    }
+        when (this.toLowerCase()) {
+            "true" -> true
+            "false" -> true
+            "t" -> true
+            "f" -> true
+            else -> false
+        }
 
 /**
  * Sanitize all mentions and replace them with their resolved discord names.
@@ -40,44 +40,41 @@ fun String.isBooleanValue() =
 fun String.sanitiseMentions(discord: Discord): String {
     val userRegex = "<@!?(\\d+)>".toRegex()
     val roleRegex = "<@&(\\d+)>".toRegex()
+    val hereRegex = "@*here".toRegex()
+    val everyone = "@*everyone".toRegex()
 
-    val mentionMap = userRegex.findAll(this).map {
-        val mention = it.value
+    val mentionMap = everyone.findAll(this).map { it.value to "everyone" } +
+            hereRegex.findAll(this).map { it.value to "here" } +
+            userRegex.findAll(this).map {
+                val mention = it.value
 
-        val resolvedName = discord.retrieveEntity { jda ->
-            jda.retrieveUserById(mention.trimToID()).complete()?.fullName()
-        } ?: mention
+                val resolvedName = discord.retrieveEntity { jda ->
+                    jda.retrieveUserById(mention.trimToID()).complete()?.fullName()
+                } ?: mention
 
-        mention to resolvedName
-    } + roleRegex.findAll(this).map {
-        val mention = it.value
+                mention to resolvedName
+            } +
+            roleRegex.findAll(this).map {
+                val mention = it.value
 
-        val resolvedName = discord.retrieveEntity { jda ->
-            jda.getRoleById(mention.trimToID())?.name
-        } ?: mention
+                val resolvedName = discord.retrieveEntity { jda ->
+                    jda.getRoleById(mention.trimToID())?.name
+                } ?: mention
 
-        mention to resolvedName
-    }
+                mention to resolvedName
+            }
 
     return replaceMap(mentionMap.toList())
-}
-
-fun String.replaceMap(replacements: List<Pair<String, String>>): String {
-    var result = this
-    replacements.forEach { (l, r) -> result = result.replace(l, r) }
-    return result
 }
 
 /**
  * Trim any type of mention into an ID.
  */
-fun String.trimToID() =
-    if (startsWith("<") && endsWith(">"))
-        this.replace("<", "")
-            .replace(">", "")
-            .replace("@", "") // User mentions
-            .replace("!", "") // User mentions with nicknames
-            .replace("&", "") // Role mentions
-            .replace("#", "") // Channel mentions
-    else
-        this
+fun String.trimToID() = takeUnless { startsWith("<") && endsWith(">") }
+        ?: replaceMap(listOf("<", ">", "@", "!", "&", "#").zip(listOf("", "", "", "", "", "")))
+
+private fun String.replaceMap(replacements: List<Pair<String, String>>): String {
+    var result = this
+    replacements.forEach { (l, r) -> result = result.replace(l, r) }
+    return result
+}
