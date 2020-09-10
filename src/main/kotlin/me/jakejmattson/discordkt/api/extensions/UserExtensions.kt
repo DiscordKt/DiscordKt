e3564b14a54dd62e6cb8fdb7282a716f46bd6021
JakeJMattson <JakeJMattson@gmail.com>
2020-09-09 22:26:04 -0500

    Add voice extensions

--- src/main/kotlin/me/jakejmattson/discordkt/api/extensions/UserExtensions.kt
+++ src/main/kotlin/me/jakejmattson/discordkt/api/extensions/UserExtensions.kt
@@ -2,22 +2,50 @@
 
 package me.jakejmattson.discordkt.api.extensions
 
+import com.gitlab.kordlib.core.behavior.*
 import com.gitlab.kordlib.core.behavior.channel.createEmbed
-import com.gitlab.kordlib.core.entity.User
+import com.gitlab.kordlib.core.entity.channel.VoiceChannel
 import com.gitlab.kordlib.rest.builder.message.EmbedBuilder
+import kotlinx.coroutines.flow.filter
 
 /**
  * Send the user a private string message.
  */
-suspend fun User.sendPrivateMessage(message: String) = getDmChannel().createMessage(message)
+suspend fun UserBehavior.sendPrivateMessage(message: String) = getDmChannel().createMessage(message)
 
 /**
  * Send the user a private embed message.
  */
-suspend fun User.sendPrivateMessage(embed: suspend EmbedBuilder.() -> Unit) = getDmChannel().createEmbed { embed.invoke(this) }
+suspend fun UserBehavior.sendPrivateMessage(embed: suspend EmbedBuilder.() -> Unit) = getDmChannel().createEmbed { embed.invoke(this) }
+
+/**
+ * Checks whether this user is the bot.
+ */
+fun UserBehavior.isSelf() = id == kord.selfId
+
+/**
+ * Checks whether this member is the bot.
+ */
+fun MemberBehavior.isSelf() = id == kord.selfId
+
+/**
+ * Move this member to a new voice channel.
+ */
+suspend fun MemberBehavior.move(channel: VoiceChannel) = edit { voiceChannelId = channel.id }
+
+/**
+ * Disconnect this member from their voice channel.
+ */
+suspend fun MemberBehavior.disconnectVoice() = edit { voiceChannelId = null }
+
+/**
+ * Get guilds shared between the user and the bot.
+ */
+val UserBehavior.mutualGuilds
+    get() = kord.guilds.filter { it.getMemberOrNull(id) != null }
 
 /**
  * A Discord profile link for this user.
  */
-val User.profileLink
+val UserBehavior.profileLink
     get() = "https://discordapp.com/users/$id/"
