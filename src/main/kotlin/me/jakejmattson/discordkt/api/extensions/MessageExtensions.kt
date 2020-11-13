5dab60714a5a80fb8e596332496c7e664c0754a1
Jake Mattson <JakeJMattson@gmail.com>
2020-11-12 23:40:39 -0600

    Add Message#jumpLink

--- src/main/kotlin/me/jakejmattson/discordkt/api/extensions/MessageExtensions.kt
+++ src/main/kotlin/me/jakejmattson/discordkt/api/extensions/MessageExtensions.kt
@@ -21,6 +21,11 @@ fun Message.containsURL() = content.containsURl()
 suspend fun Message.mentionsSomeone() = (mentionsEveryone || mentionedUsers.count() > 0 || mentionedRoles.count() > 0)
 
 /**
+ * Determine the Discord URL for this message (or null if in a DM).
+ */
+suspend fun Message.jumpLink() = getGuildOrNull()?.let { "https://discord.com/channels/${it.id.value}/${channel.id.value}/${id.value}" }
+
+/**
  * Checks whether or not this message has an attached image and has no text.
  */
 fun Message.isImagePost() =
