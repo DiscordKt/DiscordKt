1ba56c88676ded7a7c0c363467eb36cfa650e7a9
JakeJMattson <dev.ioparameters@simplelogin.fr>
2021-07-10 11:35:12 -0500

    Add MessageChannelBehavior#createMenu

--- src/main/kotlin/me/jakejmattson/discordkt/api/extensions/ChannelExtensions.kt
+++ src/main/kotlin/me/jakejmattson/discordkt/api/extensions/ChannelExtensions.kt
@@ -2,14 +2,17 @@
 
 package me.jakejmattson.discordkt.api.extensions
 
+import dev.kord.core.behavior.channel.MessageChannelBehavior
 import dev.kord.core.behavior.createTextChannel
 import dev.kord.core.behavior.createVoiceChannel
 import dev.kord.core.behavior.edit
+import dev.kord.core.entity.Message
 import dev.kord.core.entity.VoiceState
 import dev.kord.core.entity.channel.Category
 import dev.kord.core.entity.channel.VoiceChannel
 import dev.kord.rest.builder.channel.TextChannelCreateBuilder
 import dev.kord.rest.builder.channel.VoiceChannelCreateBuilder
+import me.jakejmattson.discordkt.api.dsl.MenuBuilder
 
 /**
  * Create a new text channel under this category.
@@ -36,3 +39,12 @@ suspend fun VoiceState.move(channel: VoiceChannel) = getMember().edit { voiceCha
  * Disconnect this member from their voice channel.
  */
 suspend fun VoiceState.disconnect() = getMember().edit { voiceChannelId = null }
+
+/**
+ * Create a new menu in a message channel.
+ */
+suspend fun MessageChannelBehavior.createMenu(construct: suspend MenuBuilder.() -> Unit): Message? {
+    val handle = MenuBuilder()
+    handle.construct()
+    return handle.build().send(this)
+}
