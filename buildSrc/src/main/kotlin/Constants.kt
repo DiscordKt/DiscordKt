de7e0d823174840d6e9fe273ca9ff48b5cdf924a
AMereBagatelle <themerebagatelle@gmail.com>
2021-12-29 14:29:10 -0500

    Fixed accidental dokka downgrade

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     const val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
 
     const val kotlin = "1.6.10"
-    const val dokka = "1.5.31"
+    const val dokka = "1.6.0"
     const val kord = "0.8.x-SNAPSHOT"
 }
 
