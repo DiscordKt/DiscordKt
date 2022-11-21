4c86c02099172ab19b0c483c9687342106031a80
Jake Mattson <dev.ioparameters@simplelogin.fr>
2022-11-20 23:22:39 -0600

    Redesign test syntax

--- src/test/kotlin/util/ArgumentTestFactory.kt
+++ src/test/kotlin/util/ArgumentTestFactory.kt
@@ -1,6 +1,7 @@
 package util
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.scopes.DescribeSpecContainerScope
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
 import io.mockk.every
@@ -19,16 +20,13 @@ private val contextMock = mockk<DiscordContext> {
     every { discord } returns discordMockk
 }
 
+class ArgTestBuilder<A, B>(private val arg: Argument<A, B>, private val spec: DescribeSpecContainerScope) {
+    suspend infix fun A.becomes(expected: B) {
+        val result = io.kotest.common.runBlocking { arg.transform(this, contextMock) }
 
-fun <A, B> DescribeSpec.generatePassTests(arg: Argument<A, B>, inputs: List<Pair<A, B>>) {
-    describe(arg::class.simplerName) {
-        inputs.forEach { (input, expected) ->
-            val result = runBlocking { arg.transform(input, contextMock) }
-
-            it("$input -> $expected") {
-                result.shouldBeTypeOf<Success<*>>()
-                result.result.shouldBe(expected)
-            }
+        spec.it("$this -> $result") {
+            result.shouldBeTypeOf<Success<*>>()
+            result.result.shouldBe(expected)
         }
     }
 }
