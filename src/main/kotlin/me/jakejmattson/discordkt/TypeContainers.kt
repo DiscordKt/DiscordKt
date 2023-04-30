b6e91d1e149fa34dffd993d6e0dd11335db5e2a9
Jake Mattson <dev.ioparameters@simplelogin.fr>
2023-04-30 10:55:08 -0500

    Add TypeContainer.toList()

--- src/main/kotlin/me/jakejmattson/discordkt/TypeContainers.kt
+++ src/main/kotlin/me/jakejmattson/discordkt/TypeContainers.kt
@@ -1,14 +1,49 @@
 package me.jakejmattson.discordkt
 
-/** A container for data that saves type information. */
-public interface TypeContainer
+import me.jakejmattson.discordkt.util.stringify
 
-public class NoArgs : TypeContainer
-public data class Args1<A>(val first: A) : TypeContainer
-public data class Args2<A, B>(val first: A, val second: B) : TypeContainer
-public data class Args3<A, B, C>(val first: A, val second: B, val third: C) : TypeContainer
-public data class Args4<A, B, C, D>(val first: A, val second: B, val third: C, val fourth: D) : TypeContainer
-public data class Args5<A, B, C, D, E>(val first: A, val second: B, val third: C, val fourth: D, val fifth: E) : TypeContainer
+/** A container for data that saves type information. */
+public interface TypeContainer {
+    /**
+     * Call [stringify] on all args and add them to a list.
+     *
+     * @return The list of args
+     */
+    public fun toList(): List<String>
+}
+
+public class NoArgs : TypeContainer {
+    override fun toList(): List<String> = emptyList()
+}
+
+public data class Args1<A>(val first: A) : TypeContainer {
+    override fun toList(): List<String> = listOf(stringify(first))
+}
+
+public data class Args2<A, B>(val first: A, val second: B) : TypeContainer {
+    override fun toList(): List<String> = listOf(
+        stringify(first), stringify(second)
+    )
+}
+
+public data class Args3<A, B, C>(val first: A, val second: B, val third: C) : TypeContainer {
+    override fun toList(): List<String> = listOf(
+        stringify(first), stringify(second), stringify(third)
+    )
+}
+
+public data class Args4<A, B, C, D>(val first: A, val second: B, val third: C, val fourth: D) : TypeContainer {
+    override fun toList(): List<String> = listOf(
+        stringify(first), stringify(second), stringify(third), stringify(fourth)
+    )
+}
+
+public data class Args5<A, B, C, D, E>(val first: A, val second: B, val third: C, val fourth: D, val fifth: E) :
+    TypeContainer {
+    override fun toList(): List<String> = listOf(
+        stringify(first), stringify(second), stringify(third), stringify(fourth), stringify(fifth)
+    )
+}
 
 internal fun bundleToContainer(arguments: List<Any?>) = when (arguments.size) {
     0 -> NoArgs()
